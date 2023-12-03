924a29419f189cd84cd5f62f611e4a087aa8d13d
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2023-12-02 21:08:28 -0500

    feat: Day 02 part 2 solved

---
+++ 02/day_02_2.py
@@ -0,0 +1,58 @@
+def main() -> int:
+    """
+    Calculate the total value of the sum of the multiplication of the minimun cubes in a game.
+
+    Returns:
+        int: The total value calculated from the game data.
+    """
+    total = 0
+
+    with open('input.txt', 'r') as file:
+
+        for game_data in file:
+            game_data = game_data.strip().split(";")
+            
+            red = []
+            green = []
+            blue = []
+
+            for group in game_data:
+
+                group = group.strip().split(",")
+                group = [cube.strip().split(maxsplit=2) for cube in group]
+
+                if len(group[0]) == 3:
+                    group[0] = group[0][-1].split(" ")
+
+                for i in group:
+                    n = int(i[0])
+                    color = i[1]
+
+
+                    if color == "red":
+                        red.append(n)
+                    elif color == "green":
+                        green.append(n)
+                    elif color == "blue":
+                        blue.append(n)
+
+                    min_red = 1
+                    min_green = 1
+                    min_blue = 1
+
+                if len(red) != 0:
+                    min_red = max(red)
+                if len(green) != 0:
+                    min_green = max(green)
+                if len(blue) != 0:
+                    min_blue = max(blue)
+
+                z = min_red * min_green * min_blue
+
+            total = total + z
+        return total
+                    
+
+
+if __name__ == "__main__":
+    print(main()) # 64097
